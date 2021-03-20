91833dd67086e74b22305d62d51694112a2ace6b
Ben Vizy <benvizy@gmail.com>
2021-03-20 11:48:57 -0600

    Update to PostgreSQL

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ from forms import CreatePostForm, RegiForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -85,7 +86,7 @@ class Comment(db.Model):
     commenter = relationship("User", back_populates="comments")
 
 
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def get_all_posts():
